a6312305e9da7bc5629c0b20489fc33cae12de8b
Hermine <herminemavagyan@gmail.com>
2022-06-15 11:25:41 -0700

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header >
+        <a href="/">myShoppingWeb</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
