d900674e966b25ecde7729a66e4a6f90ce07067c
Hermine <herminemavagyan@gmail.com>
2022-07-11 14:16:09 -0700

    backend server added

---
+++ backend/data.js
@@ -0,0 +1,57 @@
+const data = {
+    products: [
+        {
+            name: 'ZARA dress',
+            slug: 'zara-dress',
+            category: 'Dresses',
+            image: '/images/p1.jpg',//679x829 px
+            price: 120,
+            countInStock: 10,
+            brand: 'ZARA',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality dress'
+
+        },
+        {
+            name: 'H&M dress',
+            slug: 'h&m-dress',
+            category: 'Dresses',
+            image: '/images/p2.jpg',
+            price: 220,
+            countInStock: 20,
+            brand: 'H&M',
+            rating: 5.05,
+            numReviews: 10,
+            description: 'Beautiful dress'
+
+        },
+        {
+            name: 'BANANA RPUBLIC pants',
+            slug: 'banana-republic-pants',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 120,
+            countInStock: 14,
+            brand: 'BANANA REPUBLIC',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality pants'
+
+        },
+        {
+            name: 'Adidas shorts',
+            slug: 'adidas-shorts',
+            category: 'Shorts',
+            image: '/images/p4.jpg',
+            price: 20,
+            countInStock: 5,
+            brand: 'ADIDAS',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality shorts'
+
+        }
+    ]
+}
+export default data;
