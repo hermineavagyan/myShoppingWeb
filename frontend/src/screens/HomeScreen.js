6ea1b963477338b4ed68953e0e9fe821498aa01b
Hermine <herminemavagyan@gmail.com>
2022-07-12 14:58:52 -0700

    Product, rating componenets added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
-
 import { useEffect, useReducer, useState } from 'react';
 import axios from 'axios';
 import logger from 'use-reducer-logger';
+import { Row, Col } from 'react-bootstrap';
+import Product from "../components/Product";
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -50,20 +51,15 @@ function HomeScreen() {
                 {loading ? (<div>...Loading</div>)
                     : error ? (<div>{error}</div>)
                         : (
-                            products.map((product) => (
-                                <div className="product" key={product.slug}>
-                                    <Link to={`/product/${product.slug}`}>
-                                        <img src={product.image} alt={product.name} />
-                                    </Link>
-                                    <div className="product_info">
-                                        <Link to={`/product/${product.slug}`}>
-                                            <p>{product.name}</p>
-                                        </Link>
-
-                                        <p><strong>${product.price}</strong></p></div>
-                                    <button>Add to cart</button>
-                                </div>
-                            )))}
+                            <Row>
+                                {products.map((product) => (
+                                    <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                                        <Product product={product}></Product>
+                                    </Col>
+                                ))
+                                }
+                            </Row>
+                        )}
             </div>
         </div>
     )
