f238f5e3b5c53e000fa6f407fd2af3bf230e8848
Hermine <herminemavagyan@gmail.com>
2022-07-12 13:52:23 -0700

    from header to Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,36 @@
 
 import './App.css';
-import { BrowserRouter, Link, Route, Routes, } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import { Container, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <header >
-          <Link to="/">myShoppingWeb</Link>
+      <div className='d-flex flex-column site-container'>
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>myShoppingWeb
+                </Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />}></Route>
-            <Route path="/" element={<HomeScreen />}></Route>
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />}></Route>
+              <Route path="/" element={<HomeScreen />}></Route>
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
