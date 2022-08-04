3ade5885548a65c6d19cbc0ef0ddab60b311114b
Hermine <herminemavagyan@gmail.com>
2022-08-04 02:04:50 -0700

    shipping address added to initial state

--- frontend/src/MyContext.js
+++ frontend/src/MyContext.js
@@ -1,57 +1,3 @@
-// import { createContext, useReducer } from "react";
-
-// export const MyContext = createContext();
-
-// const initialState = {
-//     userInfo: localStorage.getItem('userInfo')
-//         ? JSON.parse(localStorage.getItem('userInfo'))
-//         : null,
-
-//     cart: {
-//         cartItems: localStorage.getItem('cartItems')
-//             ? JSON.parse(localStorage.getItem('cartItems'))
-//             : [],
-//     },
-// };
-
-// function reducer(state, action) {
-//     switch (action.type) {
-//         case 'CART_ADD_ITEM':
-//             //add to cart 
-//             const newItem = action.payload;
-//             const itemAvailable = state.cart.cartItems.find((item) => item._id === newItem._id);
-
-//             const cartItems = itemAvailable
-//                 ? state.cart.cartItems.map((item) =>
-//                     item._id === itemAvailable._id ? newItem : item)
-//                 : [...state.cart.cartItems, newItem];
-//             localStorage.setItem('cartItems', JSON.stringify(cartItems));
-//             return {
-//                 ...state, cart: { ...state.cart, cartItems }
-//             };
-//         case 'CART_REMOVE_ITEM': {
-//             const cartItems = state.cart.cartItems.filter((item) => item._id !== action.payload._id);
-//             localStorage.setItem('cartItems', JSON.stringify(cartItems));
-//             return {
-//                 ...state, cart: { ...state.cart, cartItems }
-//             };
-//         }
-//         case 'USER_SIGNIN':
-//             return { ...state, userInfo: action.payload, };
-
-//         case 'USER_SIGNOUT':
-//             return { ...state, userInfo: null, };
-//         default:
-//             return state;
-//     }
-// }
-// export function MyContextProvider(props) {
-//     const [state, dispatch] = useReducer(reducer, initialState);
-//     const value = { state, dispatch };
-//     return <MyContext.Provider value={value}>{props.children}</MyContext.Provider>
-
-
-// }
 
 import { createContext, useReducer } from 'react';
 export const MyContext = createContext();
@@ -62,6 +8,10 @@ const initialState = {
     //     : null,
 
     cart: {
+        shippingAddress: localStorage.getItem('shippingAddress')
+            ? JSON.parse(localStorage.getItem('shippingAddress'))
+            : {},
+
         cartItems: localStorage.getItem('cartItems')
             ? JSON.parse(localStorage.getItem('cartItems'))
             : [],
@@ -95,6 +45,18 @@ function reducer(state, action) {
             return {
                 ...state,
                 userInfo: null,
+                cart: {
+                    cartItems: [],
+                    shippingAddress: {}
+                },
+            };
+        case 'SAVE_SHIPPING_ADDRESS':
+            return {
+                ...state,
+                cart: {
+                    ...state.cart,
+                    shippingAddress: action.payload,
+                }
             };
         default:
             return state;
