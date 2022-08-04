92ab3078e5a492605e26d791d0b6bd3d84e0413f
Hermine <herminemavagyan@gmail.com>
2022-08-04 02:51:28 -0700

    signup componenet added

---
+++ frontend/src/screens/SignupScreen.js
@@ -0,0 +1,92 @@
+import { Helmet } from "react-helmet-async";
+import { Button, Container, Form, Toast } from 'react-bootstrap';
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useState, useContext, useEffect } from "react";
+import Axios from 'axios';
+import { MyContext } from '../MyContext';
+import { toast } from 'react-toastify';
+
+
+
+export default function SignupScreen() {
+    const navigate = useNavigate();
+    const { search } = useLocation();
+    const redirectInUrl = new URLSearchParams(search).get('redirect');
+    const redirect = redirectInUrl ? redirectInUrl : '/'
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const { state, dispatch: contextDispatch } = useContext(MyContext)
+    const { userInfo } = state;
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        if (password !== confirmPassword) {
+            toast.error('Passwords do not match');
+            return;
+        }
+        try {
+            const { data } = await Axios.post('/api/users/signup', {
+                name,
+                email,
+                password
+            });
+            contextDispatch({ type: 'USER_SIGNIN', payload: data })
+            console.log(data);
+            localStorage.setItem('userInfo', JSON.stringify(data));
+            console.log(userInfo)
+            navigate(redirect || '/');
+        } catch (error) {
+            alert("Invalid email or password")
+        }
+    }
+    useEffect(() => {
+        if (userInfo) {
+            navigate(redirect);
+        }
+    }, [navigate, redirect, userInfo]);
+
+    return (
+        <Container className="small-container">
+            <Helmet>
+                <title>Sign up</title>
+            </Helmet>
+            <h1 className="my-3"> Sign Up</h1>
+            <Form onSubmit={submitHandler}>
+
+                <Form.Group className="mb-3" controlId="name">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" required onChange={(e) => setName(e.target.value)} />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" required onChange={(e) => setEmail(e.target.value)} />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" required onChange={(e) => setPassword(e.target.value)} />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="confirmPassword">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type="password" required onChange={(e) => setConfirmPassword(e.target.value)} />
+                </Form.Group>
+
+                <div className="mb-3">
+                    <Button type="submit">Sign Up</Button>
+                </div>
+                <div className="mb-3">
+                    Already have an account? {" "}
+                    <Link to={`/signin?redirect=${redirect}`}>Sign In</Link>
+                    {/* <Link to={`/signup ? redirect = ${redirect}`}>Create account</Link> */}
+                </div>
+
+            </Form>
+        </Container>
+    )
+}
