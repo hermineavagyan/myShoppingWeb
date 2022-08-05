ab612f2f6fb3913c57341d815043758650970149
Hermine <herminemavagyan@gmail.com>
2022-08-05 08:55:53 -0700

    order preview feature added

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -2,15 +2,32 @@ import React from 'react';
 import CheckOutSteps from '../components/CheckoutSteps';
 import { Helmet } from "react-helmet-async";
 import { Row, Col, Card, Button, ListGroup } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState, useContext, useEffect } from "react";
 import { MyContext } from '../MyContext';
-import { useNavigate } from "react-router-dom";
+
 
 export default function PlaceOrderScreen() {
 
     const { state, dispatch: contextDispatch } = useContext(MyContext)
     const { cart, userInfo } = state
+    const navigate = useNavigate();
+
+    const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100;
+
+    cart.itemsPrice = round2(
+        cart.cartItems.reduce((a, c) => a + c.quantity * c.price, 0));
+    cart.shippingPrice = cart.itemPrice > 100 ? round2(0) : round2(10);
+    cart.taxPrice = round2(0.15 * cart.itemsPrice);
+    cart.totalPrice = cart.itemPrice + cart.shippingPrice + cart.taxPrice;
+
+    const placeOrderHandler = async () => { };
+
+    useEffect(() => {
+        if (!cart.paymentMethod) {
+            navigate('/payment')
+        }
+    }, [cart, navigate]);
 
     return (
         <div>
@@ -33,8 +50,80 @@ export default function PlaceOrderScreen() {
                             <Link to="/shipping">Edit</Link>
                         </Card.Body>
                     </Card>
+                    <Card className="mb-3">
+                        <Card.Body>
+                            <Card.Title>Payment</Card.Title>
+                            <Card.Text>
+                                <strong>Method:</strong> {cart.paymentMethod}
+
+                            </Card.Text>
+                            <Link to="/payment">Edit</Link>
+                        </Card.Body>
+                    </Card>
+                    <Card className="mb-3">
+                        <Card.Body>
+                            <Card.Title>Items</Card.Title>
+                            <ListGroup variant="flush">
+                                {
+                                    cart.cartItems.map((item) => (
+                                        <ListGroup.Item key={item._id}>
+                                            <Row className="align-items-center">
+                                                <Col md={6}>
+                                                    <img src={item.image} alt={item.name}
+                                                        className="img-fluid rounded img-thumbnail"></img>{' '}
+                                                    <Link to={`/product/${item.slug}`}>{item.name}</Link>
+                                                </Col>
+                                                <Col md={3}><span>{item.quantity}</span></Col>
+                                                <Col md={3}>$ {item.price}</Col>
+                                            </Row>
+                                        </ListGroup.Item>
+                                    ))}
+                            </ListGroup>
+                            <Link to="/cart">Edit</Link>
+                        </Card.Body>
+                    </Card>
                 </Col>
-                <Col md={4}></Col>
+                <Col md={4}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Order Summary</Card.Title>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Items</Col>
+                                        <Col>${cart.itemsPrice.toFixed(2)}</Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Shipping</Col>
+                                        <Col>${cart.shippingPrice.toFixed(2)}</Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Tax</Col>
+                                        <Col>${cart.taxPrice.toFixed(2)}</Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col><strong>Order Total</strong></Col>
+                                        <Col><strong>${cart.totalPrice.toFixed(2)}</strong></Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <div className="d-grid">
+                                        <Button
+                                            type="button"
+                                            onClick={placeOrderHandler}
+                                            disabled={cart.cartItems.length === 0}>Place Order</Button>
+                                    </div>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Card.Body>
+                    </Card>
+                </Col>
             </Row>
         </div>
     )
