220680ecbe0b7da151b8e7d89ead5546efbe95db
Hermine <herminemavagyan@gmail.com>
2022-08-20 08:01:39 -0700

    get order route created

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -8,22 +8,8 @@ import Order from '../models/orderModel.js';
 
 const orderRouter = express.Router();
 
-// orderRouter.post('/', isAuth, expressAsyncHandler(async (req, res) => {
-//     const newOrder = new Order({
-//         orderItems: req.body.orderItems.map((x) => ({ ...x, product: x._id })),
-//         shippingAddress: req.body.shippingAddress,
-//         paymentMethod: req.body.paymentMethod,
-//         itemsPrice: req.body.itemsPrice,
-//         shippingPrice: req.body.shippingPrice,
-//         taxPrice: req.body.taxPrice,
-//         totalPrice: req.body.totalPrice,
-//         user: req.user._id,
-//     });
-//     const order = await newOrder.save();
-//     res.status(201).send({ messsage: "New Order Created", order })
-// }))
 
-orderRouter.get(
+orderRouter.post(
     '/',
     isAuth,
     expressAsyncHandler(async (req, res) => {
@@ -40,6 +26,18 @@ orderRouter.get(
 
         const order = await newOrder.save();
         res.status(201).send({ message: 'New Order Created', order });
+    })
+);
+orderRouter.get(
+    '/:id',
+    isAuth,
+    expressAsyncHandler(async (req, res) => {
+        const order = await Order.findById(req.params.id);
+        if (order) {
+            res.send(order);
+        } else {
+            res.status(404).send({ message: "Order not found" })
+        }
     })
 );
 export default orderRouter;
